83defcea8a7cf9c9a0184218ae21ee4e1fc1c336
RAHUL GARG <garg.rahulcs@gmail.com>
2021-05-24 09:06:11 -0400

    removed redis from db.lua

--- db.lua
+++ db.lua
@@ -31,20 +31,5 @@ function _M.connect()
     return db
 end
 
-function _M.redconnect()
-
-    redis = require("resty.redis")                    -- Introduce Redis 
-    red = redis:new()
-    red:set_timeout(2000)
-    local ok, err = red:connect("127.0.0.1", 6379)
-
-    if not ok then
-            ngx.say("failed to connect: ", err, ": ", errcode, " ", sqlstate)
-        return
-    end
-    return red
-
-end
-
 return _M
         
