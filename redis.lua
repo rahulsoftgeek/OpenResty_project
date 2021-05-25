4df151f59b1a5713c06d5dbf038e87a714d33fe9
RAHUL GARG <garg.rahulcs@gmail.com>
2021-05-25 16:27:57 -0400

    Added connection pool

--- redis.lua
+++ redis.lua
@@ -1,11 +1,14 @@
+local redis = require "resty.redis"                  -- Introduce Redis 
+
 local _M={}
 
 function _M.redconnect()
 
-    redis = require("resty.redis")                    -- Introduce Redis 
-    red = redis:new()
+    local red_con = {pool = "red_conn_pool", pool_size = 100}
+
+    local red = redis:new()
     red:set_timeout(2000)
-    local ok, err = red:connect("172.31.29.85", 6379)
+    local ok, err = red:connect("127.0.0.1", 6379, red_con)
 
     if not ok then
             ngx.say("failed to connect: ", err, ": ", errcode, " ", sqlstate)
@@ -17,4 +20,3 @@ function _M.redconnect()
 end
 
 return _M
-        
